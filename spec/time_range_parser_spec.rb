e8857c98feace5083132be45145e05b467399067
Kajetan Bojko <kajboj@gmail.com>
2012-10-20 12:15:24 +0000

    parser specs pass

--- spec/time_range_parser_spec.rb
+++ spec/time_range_parser_spec.rb
@@ -2,27 +2,33 @@ require_relative 'spec_helper'
 require_relative '../time_range_parser'
 
 describe 'TimeRangeParser.parse' do
-  let(:now) { Time.local(2012, 07, 16, 10, 00, 00) }
+  let(:now) { Time.new(2012, 07, 16, 10, 00, 00) }
   before(:each) { Timecop.freeze now }
 
   subject(:time_range) { TimeRangeParser.parse bar }
 
-  context 'with valid non empty timebar' do
+  context 'with valid timebar' do
     {
-      '-O-' => '16 08:00 - 16 16:00',
-      'O--' => '16 00:00 - 16 08:00',
-      '--O' => '16 16:00 - 17 00:00',
-      'OO-' => '16 00:00 - 16 16:00',
-      '-OO' => '16 08:00 - 17 00:00',
-      'OOO' => '16 00:00 - 17 00:00',
+      'O'   => '16 00:00:00 - 17 00:00:00',
+      '-O'  => '16 12:00:00 - 17 00:00:00',
+      'O-'  => '16 00:00:00 - 16 12:00:00',
+      'OO'  => '16 00:00:00 - 17 00:00:00',
 
-      '--O--' => '16 09:36 - 16 14:24',
+      '-O-' => '16 08:00:00 - 16 16:00:00',
+      'O--' => '16 00:00:00 - 16 08:00:00',
+      '--O' => '16 16:00:00 - 17 00:00:00',
+      'OO-' => '16 00:00:00 - 16 16:00:00',
+      '-OO' => '16 08:00:00 - 17 00:00:00',
+      'OOO' => '16 00:00:00 - 17 00:00:00',
+
+      '--OO-' => '16 09:36:00 - 16 19:12:00',
+      '- X? ' => '16 09:36:00 - 16 19:12:00',
     }.each do |timebar, range|
       start, finish = range.split('-').map(&:strip)
       context timebar do
         let(:bar) { timebar }
 
-        it "is #{range} for today" do
+        it "is parsed to #{range}" do
           time_range.should == tr("2012-07-#{start}", "2012-07-#{finish}")
         end
       end
@@ -40,12 +46,20 @@ describe 'TimeRangeParser.parse' do
   end
 
   context 'with invalid time bar' do
-    let(:bar) { 'O-O' }
+    [
+      'O-O',
+      'O-O-O',
+      'i n- v- a li-d'
+    ].each do |timebar|
+      context timebar do
+        let(:bar) { timebar }
 
-    specify do
-      lambda do
-        subject
-      end.should raise_error ArgumentError
+        specify do
+          lambda do
+            subject
+          end.should raise_error ArgumentError
+        end
+      end
     end
   end
 end
--- time_range_parser.rb
+++ time_range_parser.rb
@@ -10,20 +10,74 @@ class TimeRangeParser
   end
 
   def parse
-    return nil if empty?
+    return nil if empty_bar?
+
+    start, finish = start_and_finish
+
+    validate_remainder finish
+
+    indexes_to_time_range start, finish
   end
 
   private
 
-  def empty?
-    chars = @bar.to_s.split('').uniq
-    chars.each do |c|
-      return false unless empty_chars.include? c
+  def validate_remainder finish
+    remaining_chars = @bar[finish..-1].split('')
+
+    remaining_chars.each do |c|
+      raise ArgumentError.new("Invalid time bar #{@bar}") unless empty? c
+    end
+  end
+
+  def indexes_to_time_range start_index, finish_index
+    args = [start_index, finish_index].map do |index|
+      bar_start + (index.to_f / length) * bar_length_in_seconds
+    end
+
+    TimeRange.new *args
+  end
+
+  def start_and_finish
+    start = nil
+    chars.each.with_index do |c, i|
+      if empty? c
+        return [start, i] if start
+      else
+        start ||= i
+      end
+    end
+    [start, length]
+  end
+
+  def length
+    @bar.length
+  end
+
+  def empty_bar?
+    chars.uniq.each do |c|
+      return false unless empty? c
     end
     true
   end
 
   def empty_chars
     [' ', '-']
+  end
+
+  def empty? char
+    empty_chars.include? char
+  end
+
+  def chars
+    @bar.to_s.split ''
+  end
+
+  def bar_start
+    t = Time.now
+    Time.new t.year, t.month, t.day, 0, 0, 0
+  end
+
+  def bar_length_in_seconds
+    24*60*60
   end
 end
